1b8cbf161f30b901bde0e83cda9bf38c6fe29582
Satya-Sreekar <satyasreekarpattaswami@gmail.com>
2026-06-28 02:07:18 +0530

    footer chagnes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -43,7 +43,7 @@ export default function Footer() {
           </ul>
         </div>
 
-        <div className="footer__col">
+        <div className="footer__col footer__col--wide">
           <span className="footer__col-label">Markets</span>
           <ul>
             <li>India</li>
